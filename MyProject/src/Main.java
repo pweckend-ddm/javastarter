0862a586d49ed0dddb7d9aaf8d331edb0812bb97
Peter Weckend <pweckend@dotdash.com>
2023-12-06 16:58:15 -0500

    Adding some initial exercises

--- MyProject/src/Main.java
+++ MyProject/src/Main.java
@@ -1,10 +1,9 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
@@ -13,5 +12,46 @@ public class Main {
             // for you, but you can always add more by pressing Cmd+F8.
             System.out.println("i = " + i);
         }
+
+        int j = 0;
+        String whileLoopOutput = "Apple";
+        while (j < 3) {
+            whileLoopOutput += "Orange";
+            j++;
+        }
+        System.out.println(whileLoopOutput);
+
+        printArithmetic();
+
+        System.out.println(getLargestValue(14, 5)); // should return 5
+
+        List<String> listOfCities = new ArrayList<>();
+        listOfCities.add("Edmonton");
+        listOfCities.add("Vancouver");
+        listOfCities.add("St John's");
+        System.out.println(makeListAlphabetical(listOfCities)); // should output [Edmonton, St John's, Vancouver]
+
+        System.out.println(containsTheLetterA("adidas")); // should return true
+        System.out.println(containsTheLetterA("nike")); // should return false
+        System.out.println(containsTheLetterA("")); // should return false
+        System.out.println(containsTheLetterA(null)); // should return false
+    }
+
+    public static void printArithmetic() {
+        int a = 7;
+        int b = 10;
+        System.out.println("a + b = " + (a + b));
+    }
+
+    public static int getLargestValue(int a, int b) {
+        return -1; // replace with your code
+    }
+
+    public static List<String> makeListAlphabetical(List<String> myList) {
+        return null; // replace with your code
+    }
+
+    public static boolean containsTheLetterA(String myString) {
+        return false; // replace with your code
     }
 }
